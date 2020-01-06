dc99630b4adb568e0870cb401a118e605602a55f
Tinashe <tinashejchiweshe@gmail.com>
2020-01-06 12:36:13 +0200

    websocket and server connected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const path = require('path')
-
-// Init express
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 // Express server setup
 const app = express()
+const server = http.createServer(app)
+
+// Configure Socket.io to work with server
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -12,7 +16,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Serve up publicDirectoryPath
 app.use(express.static(publicDirectoryPath))
 
+io.on('connection', () => {
+    console.log('Finally! New WebSocket connection')
+})
+
 // Listen on port 3000
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server now running on: ${port}`)
 })
