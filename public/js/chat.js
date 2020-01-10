8c75168021149668de6936e6453557a03864c41b
Tinashe <tinashejchiweshe@gmail.com>
2020-01-10 12:20:31 +0200

    init socket.io, events, broadcasting events, sharing location, event acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,35 @@
-io()
+const socket = io()
+
+socket.on('message', message => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, error => {
+        if (error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered.')
+    })
+})
+
+document.querySelector('#share-location').addEventListener('click', () => {
+    if (!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition(position => {
+        
+        socket.emit('shareLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared.')
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 // Express server setup
 const app = express()
@@ -16,8 +17,32 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // Serve up publicDirectoryPath
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', socket => {
     console.log('Finally! New WebSocket connection')
+
+    socket.emit('message', 'Welcome to Chat-chat')
+    socket.broadcast.emit('message', 'New user has joined!')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)){
+            return callback('Profanity is not permitted!')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('shareLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left the chat.')
+    })
+
 })
 
 // Listen on port 3000
